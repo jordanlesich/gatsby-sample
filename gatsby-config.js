512e1de4e736ffa99f63548c0597e2ed1125e9aa
Jordan Lesich <jordan.lesich@gmail.com>
2020-03-14 11:46:54 -0400

    added sitemap, netlifyCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,11 @@ module.exports = {
     title: `Jordan Lesich's Blog`,
     description: 'Articles about Green Web Development in Toronto',
     author: `@gatsbyjs`,
+    siteUrl: 'https://elastic-mirzakhani-9cbc0b.netlify.com/',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-source-filesystem`,
@@ -36,6 +38,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify-cms`,
     'gatsby-plugin-netlify'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
